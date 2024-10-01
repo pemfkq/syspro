eaaf9b8eaba9c813e32a32e1194128cd4799bd3f
pemfkq <dochim66@gmail.com>
2024-10-01 04:52:02 +0000

    20240930_chap03

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 #include "copy.h"
-char line[MAXLINE];
-char longest[MAXLINE]; 
+char line[50][MAXLINE];
+char Str[50][MAXLINE];
 void copy(char from[], char to[]);
-
-// change code!
 
 int main()
 {
-	int len, max = 0;
-	while (fgets(line,MAXLINE,stdin) != NULL) {
-		len = strlen(line);
-		if (len > max) {
-			max = len;
-			copy(line, longest);
-		}
+	int len = 0; // length of input text
+	int textlen[50]; // save each text length
+
+	int longestTextLen; // save the longest text's length
+	int longest = 0; // save the longest text's array number
+	int i = 0;
+
+	while (fgets(line[i],MAXLINE,stdin) != NULL) {
+		len = strlen(line[i]) - 1;
+		if(len == 0)   // I dot't know why, but NULL check is not working
+			break;  
+		textlen[i]=len;
+		copy(line[i], Str[i]);
+		i++;
+
 	}
-	if (max > 0) 
-		printf("%s", longest);
+
+	for(int n=0;n<i;n++){
+		longestTextLen = 0;
+		for(int m=0;m<i;m++){
+			if(longestTextLen < textlen[m]){
+				longest = m;
+				longestTextLen = textlen[m];
+			}
+		}
+		printf("%s", Str[longest]);
+		textlen[longest] = -1;
+	}
 	return 0;
 }
