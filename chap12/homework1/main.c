a5fcf621f3ef5af5fe6c1706dff14413dab69d70
pemfkq <dochim66@gmail.com>
2024-12-08 16:11:08 +0000

    good

---
+++ chap12/homework1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#define MAXLINE 100
+
+int main()
+{
+	int n, length, fd[2], pid;
+	char message[MAXLINE], line[MAXLINE];
+
+	pipe(fd);
+
+	if((pid = fork()) == 0)
+	{
+		close(fd[0]);
+		printf("Send: [%d] Hello from PID %d\n",getpid(),getpid());
+	}else{
+		close(fd[1]);
+		n = read(fd[1],line,MAXLINE);
+		printf("Recv: [%d] Hello from PID %d\n", getpid(),getpid());
+	}
+
+	exit(0);
+}
+
+
+
+
---
+++ chap12/prob2/stdpipe.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define MAXLINE 100
+
+int main()
+{
+	int n, pid, fd[2];
+	char line[MAXLINE];
+
+	pipe(fd);
+
+	if((pid = fork()) == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1],1);
+		close(fd[1]);
+		printf("Hello! pipe\n");
+		printf("Bye! pipe\n");
+	}else{
+		close(fd[1]);
+		printf("Return from Child Process\n");
+		while((n = read(fd[0],line,MAXLINE))>0)
+			write(STDOUT_FILENO,line,n);
+	}
+
+	exit(0);
+}
+
+
+
+
